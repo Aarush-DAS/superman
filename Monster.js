05a606feb75d64703824fd682bb3fe0c59bb50f9
Aarush-DAS <78961766+Aarush-DAS@users.noreply.github.com>
2021-07-30 16:45:14 +0530

    Add files via upload

---
+++ Monster.js
@@ -0,0 +1,23 @@
+class Monster{
+    constructor(x,y,width,height){
+var options={
+    density:10,
+    friction:0.1,
+   
+}
+this.image=loadImage("Monster1.png")
+this.body=Bodies.rectangle(x,y,width,height,options)
+
+this.width=width
+this.height=height;
+World.add(world,this.body)
+    }
+    display(){
+        
+        push();
+        translate(this.body.position.x,this.body.position.y)
+      
+       image(this.image,0,0,this.width,this.height)
+        pop()
+    }
+}
